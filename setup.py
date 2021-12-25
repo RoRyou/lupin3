bb5ec9630196bfa6e9f32230134276adcfd41a54
RoRyou <luliang_penn@live.com>
2021-12-25 17:09:10 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='lupin3',
+    version='0.0.1',
+    author='Roryou',
+    author_email='luliang_penn@live.com',
+    url='',
+    description=u'use by myself',
+    packages=['lupin4'],
+    install_requires=[],
+    
+)
